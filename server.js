d568234c41b8061df664fbe082a31fb9596f543c
Manjilan <nakarmimanjila@yahoo.com>
2018-03-12 11:17:23 -0700

    basic structure created

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var bodyParser = require("body-parser");
+var db = require("./models");
+
+app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configure app
+app.set('views', __dirname + 'views');      // Views directory
+app.use(express.static('public'));          // Static directory
+app.use(bodyParser.urlencoded({ extended: true })); // req.body
+
+// Set CORS Headers
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+app.get('/', function (req, res) {
+  res.sendFile('views/index.html' , { root : __dirname});
+});
+
+app.listen(3000, function() {
+    console.log("Server running on port 3000...")
+});
