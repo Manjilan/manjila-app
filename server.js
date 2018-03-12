c2c1be15591fdb83e9bcffc6b2f00d5fdb5ba76b
Manjilan <nakarmimanjila@yahoo.com>
2018-03-12 12:12:35 -0700

    adding initial data

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app = express();
 var bodyParser = require("body-parser");
 var db = require("./models");
 
+var personal = [];
 app.use(express.static('public'));
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -22,6 +23,26 @@ app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+app.get('/api', function apiIndex(req, res) {
+  // TODO: Document all your api endpoints below as a simple hardcoded JSON object.
+  // It would be seriously overkill to save any of this to your database.
+  // But you should change almost every line of this response.
+  res.json({
+    woopsIForgotToDocumentAllMyEndpoints: true, // CHANGE ME ;)
+    message: "Welcome to my personal api! Here's what you need to know!",
+    documentationUrl: "https://github.com/example-username/express-personal-api/README.md", // CHANGE ME
+    baseUrl: "http://YOUR-APP-NAME.herokuapp.com", // CHANGE ME
+    endpoints: [
+      {method: "GET", path: "/api", description: "Describes all available endpoints"},
+      {method: "GET", path: "/api/profile", description: "Data about me"}, // CHANGE ME
+      {method: "POST", path: "/api/campsites", description: "E.g. Create a new campsite"} // CHANGE ME
+    ]
+  })
+});
+
+
+
+//Server Commands
 app.listen((process.env.PORT || 3000), function() {
     console.log("Server running on port 3000...")
 });
