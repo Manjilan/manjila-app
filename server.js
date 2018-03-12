d1778b1fd313b660b987d10e28507d989e19f4fd
Manjilan <nakarmimanjila@yahoo.com>
2018-03-12 11:35:46 -0700

    heroku deployment take 2

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static('public'));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Configure app
-app.set('views', __dirname + 'views');      // Views directory
+app.set('views', __dirname + '/views');      // Views directory
 app.use(express.static('public'));          // Static directory
 app.use(bodyParser.urlencoded({ extended: true })); // req.body
 
@@ -22,6 +22,6 @@ app.get('/', function (req, res) {
   res.sendFile('views/index.html' , { root : __dirname});
 });
 
-app.listen(3000, function() {
+app.listen(process.env.PORT || 3000), function() {
     console.log("Server running on port 3000...")
 });
